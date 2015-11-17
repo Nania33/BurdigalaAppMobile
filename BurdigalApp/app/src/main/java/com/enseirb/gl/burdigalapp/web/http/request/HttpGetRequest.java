c2bdc20937b087d3e9446c7bd97336ca71fefdb5
RomainChabot <rchabot@enseirb.fr>
2015-11-17 22:14:17 +0100

    New packages

---
+++ BurdigalApp/app/src/main/java/com/enseirb/gl/burdigalapp/web/http/request/HttpGetRequest.java
@@ -0,0 +1,13 @@
+package com.enseirb.gl.burdigalapp.web.http.request;
+
+import com.enseirb.gl.burdigalapp.web.http.response.WebResponse;
+
+/**
+ * Created by rchabot on 17/11/15.
+ */
+public class HttpGetRequest implements HttpRequest{
+    @Override
+    public WebResponse execute() {
+        return null;
+    }
+}
---
+++ BurdigalApp/app/src/main/java/com/enseirb/gl/burdigalapp/web/http/request/HttpRequest.java
@@ -0,0 +1,10 @@
+package com.enseirb.gl.burdigalapp.web.http.request;
+
+import com.enseirb.gl.burdigalapp.web.http.response.WebResponse;
+
+/**
+ * Created by rchabot on 17/11/15.
+ */
+public interface HttpRequest {
+    WebResponse execute();
+}
