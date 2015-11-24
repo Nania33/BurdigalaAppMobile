ce57f1cfe519a3873931b849a1b9d5202345dabc
Alexandre <alraffin@ipb.fr>
2015-11-24 09:48:17 +0100

    oubli de GardenDTO

--- BurdigalApp/app/src/main/java/com/enseirb/gl/burdigalaapp/dto/GardenDTO.java
+++ BurdigalApp/app/src/main/java/com/enseirb/gl/burdigalaapp/dto/GardenDTO.java
@@ -4,4 +4,29 @@ package com.enseirb.gl.burdigalaapp.dto;
  * Created by rchabot on 17/11/15.
  */
 public class GardenDTO {
+    private String Name;
+    private String ParcType;
+    private String Use;
+    private String GestionType;
+    private String Label;
+    private PointS Point;
+
+    public GardenDTO(String name, String type, String use, String gestionType, String label, PointS point){
+        this.Name = name;
+        this.Point = point;
+        this.ParcType = type;
+        this.Use = use;
+        this.GestionType = gestionType;
+        this.Label = label;
+    }
+
+    @Override
+    public String toString(){
+        return  "Name: " + Name + "\n" +
+                Point.toString() +
+                "Parc Type: " + ParcType + "\n" +
+                "Use:" + Use + "\n" +
+                "Gestion Type: " + GestionType + "\n" +
+                "Label: " + Label + "\n\n";
+    }
 }
