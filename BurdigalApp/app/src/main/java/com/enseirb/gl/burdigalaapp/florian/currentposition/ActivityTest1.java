ef5e3b088f1d175137f1c04130eba42682a4b9db
Florian Chouarain <fchouara@enseirb-matmeca.fr>
2015-11-29 23:48:59 +0100

    Tentative de trouver la position de l'utilisateur et filtre (non totalement terminé) des n points les plus proches de l'utilisateur

---
+++ BurdigalApp/app/src/main/java/com/enseirb/gl/burdigalaapp/florian/currentposition/ActivityTest1.java
@@ -0,0 +1,66 @@
+package com.enseirb.gl.burdigalaapp.florian.currentposition;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ActivityTest1 extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        }
+    }
+
+    private void setUpMap() {
+        mMap.setMyLocationEnabled(true);
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        double latitude = myLocation.getLatitude();
+
+        double longitude = myLocation.getLongitude();
+
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        setUpMap();
+    }
+
+
+}
---
+++ BurdigalApp/app/src/main/java/com/enseirb/gl/burdigalaapp/florian/nearestPointsFilter.java
@@ -0,0 +1,73 @@
+package com.enseirb.gl.burdigalaapp.florian;
+
+import com.enseirb.gl.burdigalaapp.filters.Filter;
+import com.enseirb.gl.burdigalaapp.model.Model;
+import com.enseirb.gl.burdigalaapp.modelContainers.ModelContainer;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by flo on 29/11/15.
+ */
+public class nearestPointsFilter implements Filter {
+    private final int N_NEAREST_LOCATIONS = 10;
+    private LatLng currentLocation;
+    private LinkedList <Integer> listPositions;
+    private LinkedList <Double> listDistances;
+
+    public nearestPointsFilter(LatLng currentLocation){
+        super();
+        this.currentLocation = currentLocation;
+    }
+
+    @Override
+    public ModelContainer filterModels(List<Model> models) {
+        List<Model> selectedModels = new LinkedList<Model>();
+        nearestPositions(models, N_NEAREST_LOCATIONS);
+        for (int i = 0 ; i<listPositions.size() ; i++){
+            selectedModels.add(models.get(listPositions.get(i)));
+        }
+        return null;
+    }
+
+
+    public void nearestPositions(List<Model> models, int numberLocations){
+        listPositions = new LinkedList();
+        listDistances = new LinkedList();
+        int i = 0;
+        while (i < models.size() && i < numberLocations){
+            insertPosition(models.get(i).getPosition(),i);
+            i ++;
+        }
+        while (i < models.size()){
+            if (computeDistanceLocation(models.get(i).getPosition()) < listDistances.get(numberLocations - 1)){
+                insertPosition(models.get(i).getPosition(), i);
+                listPositions.remove(numberLocations - 1);
+                listDistances.remove(numberLocations - 1);
+            }
+            i ++;
+        }
+    }
+
+    public void insertPosition(LatLng position, int positionModel){
+        int i = 0;
+        double distance = computeDistanceLocation(position);
+        while (i < listDistances.size() && distance > listDistances.get(i)){
+            i++;
+        }
+        listDistances.add(i,distance);
+        listPositions.add(i,positionModel);
+
+    }
+
+    public double computeDistanceLocation(LatLng position){
+        double ourLatitude = currentLocation.latitude;
+        double ourLongitude = currentLocation.longitude;
+        double latitude = position.latitude;
+        double longitude = position.longitude;
+        double distance = Math.sqrt(Math.pow((latitude - ourLatitude), 2) + Math.pow((longitude - ourLongitude), 2));
+        return distance;
+    }
+}
